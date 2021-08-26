fa6e56a5f2fa6a459feb5cf8e1ce1fa2e7863336
朱亮 <teamide@163.com>
2021-08-26 18:35:32 +0800

    各自模块添加Configuration

--- redis/src/main/java/com/teamide/toolbox/redis/RedisAutoConfiguration.java
+++ redis/src/main/java/com/teamide/toolbox/redis/RedisAutoConfiguration.java
@@ -1,5 +1,7 @@
 package com.teamide.toolbox.redis;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author 朱亮
  * @version 1.0.0
@@ -7,5 +9,6 @@ package com.teamide.toolbox.redis;
  * @Description TODO
  * @createTime 2021年08月26日 17:00:00
  */
+@Configuration
 public class RedisAutoConfiguration {
 }
--- zookeeper/src/main/java/com/teamide/toolbox/zookeeper/ZookeeperAutoConfiguration.java
+++ zookeeper/src/main/java/com/teamide/toolbox/zookeeper/ZookeeperAutoConfiguration.java
@@ -1,5 +1,7 @@
 package com.teamide.toolbox.zookeeper;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author 朱亮
  * @version 1.0.0
@@ -7,5 +9,6 @@ package com.teamide.toolbox.zookeeper;
  * @Description TODO
  * @createTime 2021年08月26日 16:59:00
  */
+@Configuration
 public class ZookeeperAutoConfiguration {
 }
